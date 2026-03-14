43fdc70d25e617231659466a5740ed71083d73d1
junn326 <count326@gmail.com>
2026-03-14 22:06:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
-
-self.addEventListener('fetch', event => {
-  // ให้ทุก request ผ่านตามปกติ ไม่ cache
-  return;
-});
+self.addEventListener('fetch', () => { return; });
